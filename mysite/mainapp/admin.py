3cee8493378bb72096a98fed89535a5d5badc839
Lincoln Ledet <lincolnledet@gmail.com>
2025-02-04 23:19:36 -0500

    html pure

--- mysite/mainapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Plant, PlantFamily, PlantSpecies, GrowthCondition
-admin.site.register(Plant)
-admin.site.register(PlantFamily)
-admin.site.register(PlantSpecies)
-admin.site.register(GrowthCondition)
-# Register your models here.
--- mysite/mainapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views #. is current directory
-
-urlpatterns = [
-path("", views.index, name="index"),
-path("plants", views.plants, name="plants"),
-path("<int:id>", views.plantinfo, name="plantinfo"),
-]
